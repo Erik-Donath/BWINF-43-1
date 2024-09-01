38fcdca01ce659080c5c0b78b4b9642c18aef79f
C42xe2 <erikdonath2006@gmail.com>
2024-09-01 13:28:22 +0200

    Die Laufzeit des Algorythmuses wird jetzt Ausgegeben.

--- Wandertag/src/main.cpp
+++ Wandertag/src/main.cpp
@@ -1,8 +1,12 @@
 // Autor: Daniel Hohmann, Helped: Erik Donath
 #include <iostream>
+#include <chrono>
 
 #include "parser.h"
 #include "algorithm.h"
+
+#define now() std::chrono::system_clock::now()
+#define duration(cast, time) std::chrono::duration_cast<cast>(time).count()
 
 int main(int argc, char* argv[]) {
     if(argc != 2) {
@@ -11,9 +15,11 @@ int main(int argc, char* argv[]) {
     }
 
     auto fileContent = parseFile(argv[1]);
+    auto start = now();
     auto result = find_best_distance(fileContent.values);
-
+    auto time = now() - start;
 
     std::cout << "Die beste Strecke hat eine Distanz von " << result.distance << "m mit maximal " << result.count << " Teilnehmern." << std::endl;
+    std::cout << "Der Algorythmus hat " << duration(std::chrono::nanoseconds, time) << "ns (" << duration(std::chrono::milliseconds, time) << "ms) gebraucht." << std::endl;
     return 0;
 }
